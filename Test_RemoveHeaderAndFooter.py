ae8cfbf39eb564f434aec1e7d3936add7741e006
Gonçalo Moreira <goncaloram@hotmail.com>
2023-01-28 01:47:39 +0000

    UnitTest adjustment

--- Test_RemoveHeaderAndFooter.py
+++ Test_RemoveHeaderAndFooter.py
@@ -39,13 +39,21 @@ def RemoveHeaderAndFooter(pdf_title, pgtxt):
 
 class TestRemoveHeaderAndFooter(unittest.TestCase):
     def test_RemoveHeaderAndFooter(self):
-        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "random string"), 1)
-        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "random string Ulysses"), 1)
-        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "1 of 1305 bla bla"), 0)
-        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "5 of 1305 bla bla Trieste-Zurich-Paris 1914-1921"), " bla bla ")
-        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "1000 of 1305 bla Ulysses bla Trieste-Zurich-Paris 1914-1921"), " bla Ulysses bla ")
-        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "wtv of 1305 bla bla Ulysses"), " bla bla ")
-        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "1305 of 1305 bla bla Ulysses"), " bla bla ")
+        self.assertEqual(RemoveHeaderAndFooter("Ulysses",
+                                               "random string"),
+                         1)
+        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "random string Ulysses"),
+                         1)
+        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "1 of 1305 bla bla"),
+                         0)
+        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "5 of 1305 bla bla Trieste-Zurich-Paris 1914-1921"),
+                         " bla bla ")
+        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "1000 of 1305 bla Ulysses bla Trieste-Zurich-Paris 1914-1921"),
+                         " bla Ulysses bla ")
+        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "wtv of 1305 bla Trieste bla Ulysses"),
+                         " bla Trieste bla ")
+        self.assertEqual(RemoveHeaderAndFooter("Ulysses", "1305 of 1305 bla bla Ulysses"),
+                         " bla bla ")
 
 if __name__ == '__main__':
     unittest.main()
